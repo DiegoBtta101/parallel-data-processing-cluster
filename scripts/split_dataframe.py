46f8eae4a84ce27993298f882d1b2c53c5615406
Diego Bautista <dbautista498@gmail.com>
2025-07-09 21:01:00 -0500

    build1

--- scripts/split_dataframe.py
+++ scripts/split_dataframe.py
@@ -1,4 +1,3 @@
-
 # split_dataframe.py
 
 import pandas as pd
@@ -7,7 +6,7 @@ import numpy as np
 # Cargar el DataFrame completo desde el volumen compartido
 df = pd.read_csv('/data/Datos_Hidrometeorologicos.csv')
 
-# Dividir el DataFrame en 3 partes y guardar cada una en el volumen compartido
+# Dividir el DataFrame en 3 partes y guardar cada una en el volumen compartido.
 df_split = np.array_split(df, 3)
 for i, part in enumerate(df_split):
     part.to_csv(f'/data/df_part_{i}.csv', index=False)
